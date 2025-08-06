779d433b1f62b029f6a9ce34c85fe6ad61023249
Pavlov <dp45199@netapp.com>
2025-08-06 11:44:25 -0400

    fr adding fmap array

--- fsm.h
+++ fsm.h
@@ -21,7 +21,7 @@ typedef void (*fsm_state_handler)(fsm_state_t* state, fsm_event_t* event);
 
 
 fsm_master_t* fsm_init();
-fsm_state_t*  fsm_init_state(fsm_master_t*, uint32_t state_enum_id, void* data);
-void fsm_register_handler(fsm_master_t* m, uint32_t state_enum_id, uint32_t event_enum_id, fsm_state_handler handler);
+fsm_state_t*  fsm_init_state(fsm_master_t* master, uint32_t state_enum_id, void* data);
+void fsm_register_handler(fsm_master_t* master, uint32_t state_enum_id, uint32_t event_enum_id, fsm_state_handler handler);
 void fsm_register_default_handler(fsm_master_t* m, uint32_t state_enum_id, fsm_state_handler handler);
-int  fsm_execute_one(fsm_master_t* m, fsm_state_t* state, fsm_event_t* event);
+int  fsm_execute_one(fsm_state_t* state, fsm_event_t* event);
