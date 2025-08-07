af1545ae54d137604685631ebff6652252849371
Pavlov <dp45199@netapp.com>
2025-08-07 11:53:53 -0400

    made lack of error handling more explicit

--- fsm.c
+++ fsm.c
@@ -18,16 +18,18 @@ fsm_state_t* fsm_create_state(fsm_master_t* master, uint32_t state_enum_id, void
 }
 
 void fsm_register_handler(fsm_master_t* master, uint32_t state_enum_id, uint32_t event_enum_id, fsm_state_handler handler){
-    fmap_add_event_handler(master->fmap, state_enum_id, event_enum_id, handler);
+    //ignoring error handling.. for now
+    (void) fmap_add_event_handler(master->fmap, state_enum_id, event_enum_id, handler);
 }
 
 void fsm_register_default_handler(fsm_master_t* m, uint32_t state_enum_id, fsm_state_handler handler){
-    fmap_add_default_handler(master->fmap, state_enum_id, handler);
+    //ignoring error handling.. for now
+    (void) fmap_add_default_handler(master->fmap, state_enum_id, handler);
 }
 
 void fsm_execute_one(fsm_state_t* state, fsm_event_t* event){
     fsm_state_handler handler = fmap_get_handler(state->master->fmap, state->enum_id, event->enum_id);
-    return handler(state, event);
+    if (handler != NULL) handler(state, event);
 }
 
 
